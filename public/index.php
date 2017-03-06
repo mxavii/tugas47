873358479eb8719a68aaf66ac795dd00ae540992
Imam Syafii <shafii@gmx.com>
2017-03-06 08:32:24 +0700

    tugas47

--- public/index.php
+++ public/index.php
@@ -1,22 +1,9 @@
 
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Halaman Utama</title>
-    </head>
-    <body>
-        <a href='index.php/mobil'>Mobil</a>
-        <a href='index.php/motor'>Motor</a>
-    </body>
-</html>
-
 <?php
 
 require '../bootstrap/app.php';
 require '../bootstrap/container.php';
-require '../routes/web.php';
+require '../routes/route.php';
 
 
 $app->run();
